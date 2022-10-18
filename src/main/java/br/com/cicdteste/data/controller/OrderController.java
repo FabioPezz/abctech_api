58ede1ac928bc9602a59fef6de41a8b7211e4382
unknown <fabio.pezzuol@gmail.com>
2022-10-18 12:39:56 -0300

    fix Lista de ordens por OperatorId

--- src/main/java/br/com/cicdteste/data/controller/OrderController.java
+++ src/main/java/br/com/cicdteste/data/controller/OrderController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -32,6 +33,7 @@ public class OrderController {
 		return ResponseEntity.ok().build();
 	}
 	
+	@GetMapping("/operator/{operatorId}")
 	public ResponseEntity<List<OrderResponseDto>> listOrdersOperator(@PathVariable Long operatorId){
 		
 		return ResponseEntity.ok(orderApplication.listOrderByOperatorId(operatorId));
